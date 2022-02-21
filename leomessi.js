94f94ee92625497ef670ac80ddcfb2875fd19def
Amir-Mangisto <mangisto.man@gmail>
2022-02-21 13:48:17 +0200

    changing to async

--- leomessi.js
+++ leomessi.js
@@ -1,17 +1,28 @@
 const celebHttp = require("http").Server;
 const fs = require('fs');
-
+const util=require('util')
+const readFilePromise=util.promisify(fs.readFile);  
 class meLeo extends celebHttp {
-    constructor(){
-        super();
-        this.listen(8080);
-        this.on('request',this.celebInfo)
-    }
-    celebInfo(req,res) {
-        fs.readFile('./myFIle.txt',(err,data)=>{
-            res.end(data)
-        })
-    }
-}
+  constructor() {
+    super();
+    this.listen(8080);
+    this.on("request", this.celebInfo);
+  }
+//   celebInfo = (req, res) => {
+//     fs.readFile("./myFile.txt", (err, data) => {
+//       if (err) {
+//         res.end("error");
+//       } else {
+//         res.end(data.toString());
+//       }
+//     });
+//   };
 
+  ///////////////async
+  celebInfo = (req, res) =>{
+    readFilePromise('./myFile.txt')
+    .then((data)=>{res.end(data)})
+    .catch((err)=>{res.end("cant get info")})
+  }
+}
 module.exports = new meLeo();
